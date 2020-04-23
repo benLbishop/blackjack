75ae73876f144539cc3b04aa1ec257d27e264401
benLbishop <ben.bishop374@gmail.com>
2020-04-23 12:49:24 -0700

    Set up the project with typescript framework and TailwindCSS.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-blue-600">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
